0a4d4e26af7aafe0cc3128dcf5169b30ecfb6d39
Bryan Rodrigues <brodrigues@weareinteractive.ca>
2018-03-19 13:32:47 -0400

    Linked GraphQL to outside API

--- Schema/schema.js
+++ Schema/schema.js
@@ -1,14 +1,8 @@
 const graphql = require('graphql');
 
-const _ = require('lodash');
+const axios = require('axios');
 
 const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
-
-const users = [
-  { id: '23', firstName: 'Bill', age: 20 },
-  { id: '47', firstName: 'Samantha', age: 21 },
-  { id: '48', firstName: 'Tom', age: 25 }
-];
 
 const UserType = new GraphQLObjectType({
   name: 'User',
@@ -30,7 +24,11 @@ const RootQuery = new GraphQLObjectType({
         }
       },
       resolve(parentValue, args) {
-        return _.find(users, { id: args.id });
+        return axios
+          .get(`http://localhost:5000/users/${args.id}`)
+          .then(result => {
+            return result.data;
+          });
       }
     }
   }
