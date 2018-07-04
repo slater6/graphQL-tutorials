25a2f8245358d59823a44b5e281235c69a393b13
Bryan Parcelz <bryan@parcelz.com>
2018-07-04 13:13:52 -0400

    Setup graphql to use cookies

--- udemy/auth/client/components/Header.js
+++ udemy/auth/client/components/Header.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+import query from '../queries/CurrentUser'
 
 class Header extends Component {
+    renderButtons() {
+        if (this.props.data.loading) {
+            return <div />
+        }
+
+        if (this.props.data.user) {
+            return <div>Logout</div>
+        } else {
+            return (
+                <div>Sign In</div>
+            )
+        }
+    }
     render() {
+
         return (
-            <div>
-                Header
-            </div>
+            <nav>
+                <div className="nav-wrapper">
+                    {this.renderButtons()}
+                </div>
+            </nav>
         )
     }
 }
 
-export default Header;
+export default graphql(query)(Header);
